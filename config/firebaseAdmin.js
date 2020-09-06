1ec60825612b382a7e885fc1c54a14fa30efc451
Tobias Sernhede <tobias@sernhede.com>
2020-09-06 21:12:04 +0200

    service-account-firebase.json

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -2,15 +2,11 @@ import * as firebaseAdmin from 'firebase-admin';
 
 // get this JSON from the Firebase board
 // you can also store the values in environment variables
-// import serviceAccount from './serviceAccountKey.json'; 
+import serviceAccount from 'service-account-firebase.json'; 
 
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert({
-      privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
-      clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-      projectId: process.env.FIREBASE_ADMIN_PROJECT_ID.replace(/\\n/g, '\n'),
-    }),
+    credential: firebaseAdmin.credential.cert(serviceAccount),
     databaseURL: "https://quister-ef44a.firebaseio.com",
   });
 }
