8b8e6efd77101822a71edf32fa3422c570920737
Tobias Sernhede <tobias@sernhede.com>
2020-09-06 20:57:55 +0200

    replace on private key

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -9,7 +9,7 @@ if (!firebaseAdmin.apps.length) {
     credential: firebaseAdmin.credential.cert({
       privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
       clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-      projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
+      projectId: process.env.FIREBASE_ADMIN_PROJECT_ID.replace(/\\n/g, '\n'),
     }),
     databaseURL: "https://quister-ef44a.firebaseio.com",
   });
